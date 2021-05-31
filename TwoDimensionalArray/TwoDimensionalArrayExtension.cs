9fdb35362792e60c83138124e0c941f91ec51441
Dmitry Sharonov <dmitry_sharonov@epam.com>
2021-06-01 00:19:25 +0300

    feat: Add two dimentional array extention

---
+++ TwoDimensionalArray/TwoDimensionalArrayExtension.cs
@@ -0,0 +1,18 @@
+﻿namespace TwoDimensionalArray
+{
+    public static class TwoDimensionalArrayExtension
+    {
+        const int rowsIndex = 0;
+        const int columnsIndex = 1;
+
+        public static int GetRowsCount(this bool[,] array)
+        {
+            return array.GetLength(rowsIndex);
+        }
+
+        public static int GetColumnsCount(this bool[,] array)
+        {
+            return array.GetLength(columnsIndex);
+        }
+    }
+}
