491ed5de4f323a5ecb22d5c35d0d7f628f47ffc5
Dmitry Sharonov <dmitry_sharonov@epam.com>
2021-06-01 00:19:50 +0300

    feat: Add truth table methods

--- MathematicalLogicProcessor/TruthTable.cs
+++ MathematicalLogicProcessor/TruthTable.cs
@@ -1,23 +1,152 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using TwoDimensionalArray;
 
 namespace MathematicalLogicProcessor
 {
     public class TruthTable
     {
-        private readonly List<Token> polishNotation;
+        private const string functionIdentifier = "Function";
+        private const int binaryBase = 2;
         private readonly List<Operand> variables;
         private readonly List<List<Token>> headers;
+        private readonly bool[] functionVector;
+        private readonly int functionNumber;
+        private readonly bool[,] table;
 
-        public List<Token> PolishNotation { get { return polishNotation; } }
         public List<Operand> Variables { get { return variables; } }
         public List<List<Token>> Headers { get { return headers; } }
+        public bool[] FunctionVector { get { return functionVector; } }
+        public int FunctionNumber { get { return functionNumber; } }
 
-        public TruthTable(List<Token> polishNotation, List<Operand> variables)
+        public TruthTable(List<Operand> variables, List<Token> polishNotation)
         {
-            this.polishNotation = polishNotation;
             this.variables = variables;
             headers = GetHeaders(polishNotation, variables);
+            table = GetTruthTable(variables, polishNotation);
+            functionVector = GetFunctionVector(table);
+        }
+
+        public TruthTable(bool[] functionVector)
+        {
+            if (CheckVectorLength(functionVector.Length))
+            {
+                this.functionVector = functionVector;
+                functionNumber = GetFunctionNumber(functionVector);
+                int variablesCount = GetVariablesCount(functionVector.Length);
+                variables = GetVariables(variablesCount);
+                headers = GetHeaders(variables);
+                table = GetTruthTable(variables, functionVector);
+            }
+            else
+            {
+                throw new ArgumentException(nameof(functionVector));
+            }
+        }
+
+        public TruthTable(int functionNumber)
+        {
+            this.functionNumber = functionNumber;
+            functionVector = GetFunctionVector(functionNumber);
+            int variablesCount = GetVariablesCount(functionVector.Length);
+            variables = GetVariables(variablesCount);
+            headers = GetHeaders(variables);
+            table = GetTruthTable(variables, functionVector);
+        }
+
+        private bool CheckVectorLength(int length)
+        {
+            if (length == 2)
+                return true;
+            else if (length % 2 == 0)
+                return CheckVectorLength(length / 2);
+            else
+                return false;
+        }
+
+        private bool[] GetFunctionVector(int functionNumber)
+        {
+            double maxNumber;
+            int variablesCount = 0;
+            double length;
+            do
+            {
+                length = Math.Pow(2, variablesCount);
+                maxNumber = Math.Pow(binaryBase, length);
+                variablesCount++;
+            }
+            while (length % 1 != 0 || maxNumber <= functionNumber);
+            int vectorLength = (int)length;
+
+            string vector = Convert.ToString(functionNumber, binaryBase);
+            vector = vector.PadLeft(vectorLength, '0');
+            bool[] functionVector = new bool[vectorLength];
+            for (int i = 0; i < vectorLength; i++)
+                functionVector[i] = vector[i] == '1';
+
+            Array.Reverse(functionVector);
+
+            return functionVector;
+        }
+
+        private bool[] GetFunctionVector(bool[,] table)
+        {
+            int lastColumnIndex = table.GetColumnsCount() - 1;
+            int rowsCount = table.GetRowsCount();
+            bool[] functionVector = new bool[rowsCount];
+            for (int i = 0; i < rowsCount; i++)
+                functionVector[i] = table[i, lastColumnIndex];
+
+            return functionVector;
+        }
+
+        private int GetFunctionNumber(bool[] functionVector)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (bool value in functionVector)
+            {
+                sb.Append(value ? 1.ToString() : 0.ToString());
+            }
+            string vector = sb.ToString();
+
+            return Convert.ToInt32(vector, binaryBase);
+        }
+
+        private int GetVariablesCount(int vectorLength)
+        {
+            return (int)Math.Log(vectorLength, binaryBase);
+        }
+
+        private List<Operand> GetVariables(int variablesCount)
+        {
+            const int alphabetStartCode = 65;
+
+            List<Operand> variables = new List<Operand>();
+            int code = alphabetStartCode;
+            for (int i = 0; i < variablesCount; i++)
+            {
+                variables.Add(new Operand(((char)code).ToString(), TokenType.Variable));
+                code++;
+            }
+
+            return variables;
+        }
+
+        private int GetLinesCount(int variablesCount)
+        {
+            return (int)Math.Pow(binaryBase, variablesCount);
+        }
+
+        private List<List<Token>> GetHeaders(List<Operand> variables)
+        {
+            List<List<Token>> headers = new List<List<Token>>();
+            foreach (Operand variable in variables)
+                headers.Add(new List<Token> { variable });
+
+            headers.Add(new List<Token> { new Token(functionIdentifier, TokenType.Variable) });
+            return headers;
         }
 
         private List<List<Token>> GetHeaders(List<Token> polishNotation, List<Operand> variables)
@@ -92,6 +221,49 @@ namespace MathematicalLogicProcessor
             newOperand.Add(new Token(")", TokenType.CloseBrace));
 
             return newOperand;
+        }
+
+        private bool[,] GetTruthTable(List<Operand> variables, bool[] functionVector)
+        {
+            int linesCount = functionVector.Length;
+            bool[,] table = new bool[linesCount, variables.Count + 1];
+            for (int i = 0; i < linesCount; i++)
+            {
+                string line = Convert.ToString(i, binaryBase).PadLeft(variables.Count, '0');
+                for (int j = 0; j < variables.Count; j++)
+                {
+                    table[i, j] = line[j] == '1';
+                }
+            }
+
+            int lastColumnIndex = variables.Count;
+            for (int i = 0; i < linesCount; i++)
+                table[i, lastColumnIndex] = functionVector[i];
+
+            return table;
+        }
+
+        private bool[,] GetTruthTable(List<Operand> variables, List<Token> polishNotation)
+        {
+            int linesCount = GetLinesCount(variables.Count);
+            bool[,] table = new bool[linesCount, polishNotation.Count];
+            for (int i = 0; i < linesCount; i++)
+            {
+                string line = Convert.ToString(i, binaryBase).PadLeft(variables.Count, '0');
+                for (int j = 0; j < variables.Count; j++)
+                {
+                    table[i, j] = line[j] == '1';
+                }
+            }
+
+            ///
+
+            return table;
+        }
+
+        public bool this[int i, int j]
+        {
+            get { return table[i, j]; }
         }
     }
 }
